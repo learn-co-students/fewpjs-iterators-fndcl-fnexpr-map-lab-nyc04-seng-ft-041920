8554906235b73294740e5ae38068502cab8b4d8e
JonTDean <JonathanTomDean@Gmail.com>
2020-06-29 06:26:06 -0400

    Done.

--- index.js
+++ index.js
@@ -11,14 +11,26 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// Commits to splitwords function and then flattens the incoming array
 const titleCased = () => {
   return tutorials.map(title => splitWords(title)).flat();
 }
 
+// Splits words in Array, maps upcase words to array then Joins them together
 function splitWords(title){
   return title.split(" ").map(upCaseWords).join(" ");
 }
 
+// Up Cases Individual letters then joins to the beginning of the array
 function upCaseWords(word){
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
+
+/*
+Sources:
+
+https://flaviocopes.com/how-to-uppercase-first-letter-javascript/
+https://stackoverflow.com/questions/16037049/why-does-javascript-map-function-return-undefined
+https://www.robinwieruch.de/javascript-map-array
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+*/
