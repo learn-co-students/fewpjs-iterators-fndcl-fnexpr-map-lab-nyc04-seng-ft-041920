a28619a77de68966746e103c53e1f6c49d85588d
residenttrickster <metkusblackus@gmail.com>
2020-07-01 16:07:18 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map (tutorial => {
+
+      const words = tutorial.split(' ')
+      const capWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      const phrase = capWords.join(' ')
+      return phrase
+  })
 }
